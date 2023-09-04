b464572e843905442c9d9d3024b7e9867f5262cf
emodot <emayeodavid@gmail.com>
2023-09-04 08:54:21 +0100

    developed products section products card components

---
+++ components/Product.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="card_list">
+    <div v-for="(card, index) in cardDetails" :key="index" class="card_ctn">
+      <div class="card_image">
+        <img :src="card.image" alt="">
+      </div>
+      <p class="card_title">
+        {{ card.name }}
+      </p>
+      <div class="card_amount">
+        <p>${{ card.price }} USD</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    cardDetails: {
+      type: Array,
+      default: () => []
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+.card_list {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+.card_ctn {
+  padding: 30px 0;
+  background-color: white;
+  border: 1px solid #99999969;
+  border-radius: 20px;
+  width: 23%;
+  cursor: pointer;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-shadow: 0px 5px 10px #dadada;
+  margin-bottom: 40px;
+  transition:  0.4s all ease-in-out;
+}
+
+.card_ctn:hover {
+  -ms-transform: scale(1.05); /* IE 9 */
+  -webkit-transform: scale(1.05); /* Safari 3-8 */
+  transform: scale(1.05);
+}
+
+.card_image {
+  width: 72%;
+}
+
+.card_image img {
+  width: 100%;
+}
+
+.card_title {
+  margin: 15px 0;
+  font-size: 15px;
+}
+.card_amount {
+  width: fit-content;
+  padding: 7px 15px;
+  border-radius: 30px;
+  background-color: var(--primary-color);
+}
+
+.card_amount p {
+  color: white;
+}
+</style>
---
+++ components/ProductsContainer.vue
@@ -0,0 +1,176 @@
+<template>
+  <div class="products_ctn">
+    <div class="top_section">
+      <h2 class="title">
+        {{ activeTab }}
+      </h2>
+      <div class="tab_section">
+        <div v-for="(tab, index) in tabs" :key="index" :class="`tab ${activeTab.toLowerCase() === tab.toLowerCase() ? 'activeTab' : ''}`" @click="setActiveTab(tab)">
+          <p>{{ tab }}</p>
+        </div>
+      </div>
+    </div>
+    <hr class="line">
+    <div class="product_list_ctn">
+      <Product :card-details="products" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      tabs: [
+        'All Toys',
+        'Wooden Toys',
+        'Stuffed Animals'
+      ],
+      activeTab: 'All Toys',
+      products: [
+        {
+          name: 'Teddy Bear',
+          price: '30.00',
+          image: require('assets/images/teddy.png')
+        },
+        {
+          name: 'Cute Dog',
+          price: '50.00',
+          image: require('assets/images/cute-dog.png')
+        },
+        {
+          name: 'Mega Plush Toy',
+          price: '100.00',
+          image: require('assets/images/toy.png')
+        },
+        {
+          name: 'Little Rabbit',
+          price: '70.00',
+          image: require('assets/images/rabbit.png')
+        },
+        {
+          name: 'Cute Dog',
+          price: '50.00',
+          image: require('assets/images/cute-dog.png')
+        },
+        {
+          name: 'Mega Plush Toy',
+          price: '100.00',
+          image: require('assets/images/toy.png')
+        },
+        {
+          name: 'Teddy Bear',
+          price: '30.00',
+          image: require('assets/images/teddy.png')
+        },
+        {
+          name: 'Mega Plush Toy',
+          price: '100.00',
+          image: require('assets/images/toy.png')
+        },
+        {
+          name: 'Little Rabbit',
+          price: '70.00',
+          image: require('assets/images/rabbit.png')
+        },
+        {
+          name: 'Cute Dog',
+          price: '50.00',
+          image: require('assets/images/cute-dog.png')
+        },
+        {
+          name: 'Teddy Bear',
+          price: '30.00',
+          image: require('assets/images/teddy.png')
+        },
+        {
+          name: 'Little Rabbit',
+          price: '70.00',
+          image: require('assets/images/rabbit.png')
+        },
+        {
+          name: 'Mega Plush Toy',
+          price: '100.00',
+          image: require('assets/images/toy.png')
+        },
+        {
+          name: 'Cute Dog',
+          price: '50.00',
+          image: require('assets/images/cute-dog.png')
+        },
+        {
+          name: 'Mega Plush Toy',
+          price: '100.00',
+          image: require('assets/images/toy.png')
+        },
+        {
+          name: 'Little Rabbit',
+          price: '70.00',
+          image: require('assets/images/rabbit.png')
+        },
+        {
+          name: 'Mega Plush Toy',
+          price: '100.00',
+          image: require('assets/images/toy.png')
+        }
+      ]
+    }
+  },
+  methods: {
+    setActiveTab (tab) {
+      this.activeTab = tab
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+.products_ctn {
+  margin-top: 30px;
+}
+
+.top_section {
+  display: flex;
+  justify-content: space-between;
+  padding: 20px 0 40px;
+  border-bottom: 1px solid var(--border-color);
+}
+
+.line {
+  /* padding-bottom: 40px; */
+  margin-top: -1px;
+  border: 1px solid var(--primary-color);
+  width: 11rem;
+  z-index: 20;
+}
+
+.tab_section {
+  display: flex;
+  align-items: center;
+}
+
+.tab {
+  padding: 10px 20px;
+  border-radius: 30px;
+  cursor: pointer;
+}
+
+.tab p {
+  font-size: 13px;
+  line-height: 13px;
+}
+
+.activeTab {
+  background-color: var(--primary-color);
+}
+
+.activeTab p {
+  color: white;
+}
+
+.product_list_ctn {
+  margin: 5rem 0;
+}
+
+</style>
